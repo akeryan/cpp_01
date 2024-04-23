48956e1925c55fba3b38334cbf3521763319ffeb
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-04-23 17:38:07 +0400

    project done

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,9 +6,12 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 14:51:30 by akeryan           #+#    #+#             */
-/*   Updated: 2024/04/20 17:18:45 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/04/23 17:24:58 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
 
 #include <string>
 
@@ -23,3 +26,9 @@ class Zombie {
 		void setName(std::string name);
 		void announce(void);
 };
+
+
+void	randomChump(std::string name); 
+Zombie	*newZombie(std::string name); 
+
+#endif
--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -1,33 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/20 16:52:40 by akeryan           #+#    #+#             */
-/*   Updated: 2024/04/23 17:23:18 by akeryan          ###   ########.fr       */
+/*   Created: 2024/04/23 17:21:27 by akeryan           #+#    #+#             */
+/*   Updated: 2024/04/23 17:21:55 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "Zombie.hpp"
 
-int main() {
-	std::string name;
-	Zombie		*z;
-
-	std::cout << std::endl;
-	std::cout << "---------- Zombie on the STACK -----------" << std::endl;
-	std::cout << "Enter zombie name: ";
-	std::cin >> name;
-	z = newZombie(name);
-	z->announce();
-
-	std::cout << "---------- Zombie on the HEAP -----------" << std::endl;
-	std::cout << "Enter zombie name: ";
-	std::cin >> name;
-	randomChump(name);
-	delete z;
-	return 0;
+Zombie *newZombie(std::string name) {
+	return new Zombie(name);
 }
